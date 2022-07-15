818b8cad1f2a74e2ae1da09c553172dd257c5a83
Kasowy_Code <maciej.smirski@hotmail.com>
2022-07-15 07:28:16 +0200

    Created basic page components and Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CalendarComponent} from "./calendar/calendar.component";
+import {LotteryComponent} from "./lottery/lottery.component";
+import {CarsComponent} from "./cars/cars.component";
+import {AccountComponent} from "./account/account.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: "Lottery", component: LotteryComponent},
+  {path: "Calendar", component: CalendarComponent},
+  {path: "Cars", component: CarsComponent},
+  {path: "Account", component: AccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatInputModule} from "@angular/material/input";
+import { MatNativeDateModule} from "@angular/material/core";
+import {MatCardModule} from "@angular/material/card";
+import { CalendarComponent } from './calendar/calendar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import { LotteryComponent } from './lottery/lottery.component';
+import { AccountComponent } from './account/account.component';
+import {CarsComponent} from "./cars/cars.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CalendarComponent,
+    LotteryComponent,
+    CarsComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
