593832b6406ef3b7e55748bffb669c2dd0a5ac43
Kasowy_Code <maciej.smirski@hotmail.com>
2022-08-01 15:43:24 +0200

    Added basic framework for working with calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
-import {RoleService} from "../role.service";
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {DateRange} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit{
-  selected: Date | null | undefined;
-  constructor(private roleService:RoleService) { }
+export class CalendarComponent{
+  selected: Date | undefined;
+  minDate: (Date&DateRange<Date>) | Date | null;
+  maxDate: (Date&DateRange<Date>) | Date | null;
 
-  ngOnInit(){
-    // DO KOMPONENTOW ADMIN ZWRACA TRUE JESLI POSIADA ADMINA
-    // TODO: Przenieść do RoleGuard
-    console.log(this.roleService.isAdmin())
+  @Input() selectedRangeValue: DateRange<Date> | undefined;
+  @Output() selectedRangeValueChange = new EventEmitter<DateRange<Date>>();
+
+  constructor() {
+    const currentDate = new Date();
+    this.minDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
+    this.maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 2, 0);
+  }
+
+  selectedChange(m: any) {
+      if (!this.selectedRangeValue?.start || this.selectedRangeValue?.end) {
+        this.selectedRangeValue = new DateRange<Date>(m, null);
+      } else {
+        const start = this.selectedRangeValue.start;
+        const end = m;
+        if (end < start) {
+          this.selectedRangeValue = new DateRange<Date>(end, start);
+        } else {
+          this.selectedRangeValue = new DateRange<Date>(start, end);
+        }
+      }
+      this.selectedRangeValueChange.emit(this.selectedRangeValue);
+      console.log(this.selectedRangeValue);
+    }
+
+  changeSelectStrategy() {
+    if(this.selectedRangeValue?.start && !this.selectedRangeValue?.end) {
+      this.selectedRangeValue = new DateRange<Date>(this.selectedRangeValue.start, this.selectedRangeValue.start);
+    }
+    console.log(this.selectedRangeValue);
   }
 }
