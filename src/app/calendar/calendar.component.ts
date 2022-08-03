4560c8cc933b5d4450f624e3c108a61b5bf09692
gohan <dominik.switlik@gmail.com>
2022-08-03 13:30:50 +0200

    cleared code

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -8,6 +8,7 @@ interface ParkingDateDTO {
   userId: number,
   date: Date
 }
+
 interface ParkingDay {
   day: number,
   month: number
@@ -19,12 +20,12 @@ interface ParkingDay {
   styleUrls: ['./calendar.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class CalendarComponent implements OnInit{
-  parkings:any = [];
+export class CalendarComponent implements OnInit {
+  parkingLots: any = [];
   hasParkingOnDays: ParkingDay[] = [];
   selected: Date | undefined;
-  minDate: (Date&DateRange<Date>) | Date | null;
-  maxDate: (Date&DateRange<Date>) | Date | null;
+  minDate: (Date & DateRange<Date>) | Date | null;
+  maxDate: (Date & DateRange<Date>) | Date | null;
   loaded = false;
   @Input() selectedRangeValue: DateRange<Date> = new DateRange<Date>(null, null);
   @Output() selectedRangeValueChange = new EventEmitter<DateRange<Date>>();
@@ -38,7 +39,9 @@ export class CalendarComponent implements OnInit{
   ngOnInit() {
     this.parkingService.getParkings().subscribe(
       (data: any) => {
-        data.forEach((el:any) => {this.parkings.push(el)});
+        data.forEach((el: any) => {
+          this.parkingLots.push(el)
+        });
       }
     )
     this.parkingService.getMyParkingPlaces().subscribe(
@@ -51,56 +54,36 @@ export class CalendarComponent implements OnInit{
 
         });
         this.loaded = true;
-        // const elements = document.querySelectorAll('.mat-calendar-body-cell-content');
-        // const current_month = new Date().getMonth();
-        // console.log(this.hasParkingOnDays);
-        // elements.forEach((x) => {
-        //   console.log({day: parseInt(x.innerHTML), month: current_month});
-        //   if(this.hasParkingOnDays.some(item => item.day == parseInt(x.innerHTML) && item.month == current_month)) {
-        //     x.classList.add('have-parking');
-        //   }
-        // })
       }
     )
-    // this.http.get(`${environment.link}/api/parking-places/my-parking-places`).subscribe(
-    //   (res: any) => {
-    //     res.forEach((data: any) => {})
-    //   }
-    // )
   }
 
   selectedChange(m: any) {
-      if (!this.selectedRangeValue?.start || this.selectedRangeValue?.end) {
-        this.selectedRangeValue = new DateRange<Date>(m, null);
+    if (!this.selectedRangeValue?.start || this.selectedRangeValue?.end) {
+      this.selectedRangeValue = new DateRange<Date>(m, null);
+    } else {
+      const start = this.selectedRangeValue.start;
+      const end = m;
+      if (end < start) {
+        this.selectedRangeValue = new DateRange<Date>(end, start);
       } else {
-        const start = this.selectedRangeValue.start;
-        const end = m;
-        if (end < start) {
-          this.selectedRangeValue = new DateRange<Date>(end, start);
-        } else {
-          this.selectedRangeValue = new DateRange<Date>(start, end);
-        }
+        this.selectedRangeValue = new DateRange<Date>(start, end);
       }
-      this.selectedRangeValueChange.emit(this.selectedRangeValue);
-      console.log(this.selectedRangeValue);
     }
+    this.selectedRangeValueChange.emit(this.selectedRangeValue);
+    console.log(this.selectedRangeValue);
+  }
 
   confirmDateRange() {
-    if(this.selectedRangeValue?.start && !this.selectedRangeValue?.end) {
+    if (this.selectedRangeValue?.start && !this.selectedRangeValue?.end) {
       this.selectedRangeValue = new DateRange<Date>(this.selectedRangeValue.start, this.selectedRangeValue.start);
     }
   }
 
-  //
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
     if (view == "month") {
       return this.hasParkingOnDays.some(item => item.day == cellDate.getDate() && item.month == cellDate.getMonth()) ? 'have-parking' : '';
     }
     return "";
   };
-
-
-  //
-
-
 }
