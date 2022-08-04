269155a854ac21d6f099ff6a03842a1233ed6ebd
Polina Haritonow <polinakharytonova@gmail.com>
2022-08-04 13:45:02 +0200

    Corrections were made

---
+++ src/app/lottery/lottery-dialog/lottery-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lottery-dialog',
+  templateUrl: './lottery-dialog.component.html',
+  styles: [
+  ]
+})
+export class LotteryDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
