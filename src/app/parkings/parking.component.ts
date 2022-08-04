928c65407ca4aff8bc52b68dad860ecd11762f02
Polina Haritonow <polinakharytonova@gmail.com>
2022-08-04 12:55:57 +0200

    Corrections were made

--- src/app/parkings/parking.component.ts
+++ src/app/parkings/parking.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {ParkingDialogComponent} from "./dialog/parking-dialog.component";
 import {ParkingLotsListService} from "../shared/services/parking-lots-list.service";
-import {FormControl} from "@angular/forms";
+import {FormControl, Validators} from "@angular/forms";
 import {ParkingLot} from "../shared/dto/parking-lot";
 
 
@@ -15,15 +15,13 @@ import {ParkingLot} from "../shared/dto/parking-lot";
 export class ParkingComponent implements OnInit {
   disabled = false;
   parkingLotsList: ParkingLot[] = [];
-  inputValue = new FormControl('');
-  numberInputValue = new FormControl('1');
+    inputValue = new FormControl('');
+    numberInputValue = new FormControl('1');
   parkingLot = <ParkingLot>{};
-
 
   constructor(public dialog: MatDialog,
               private parkingService: ParkingLotsListService) {
   }
-
 
   ngOnInit(): void {
     this.getParkingLotsList();
@@ -45,22 +43,21 @@ export class ParkingComponent implements OnInit {
   }
 
   addParkingLot() {
-    if (this.inputValue.value != null) {
+    // @ts-ignore
+    if (this.inputValue.value != null && this.numberInputValue.value > 0 && this.inputValue.value != '') {
       this.parkingLot.name = this.inputValue.value;
-    }
-    if (this.numberInputValue.value != null) {
       this.parkingLot.parkingPlaceCount = Number(this.numberInputValue.value);
-    }
+
 
     this.parkingLot.isAvailable = false;
-
     console.log(this.parkingLot)
-    this.parkingService.addParkingLot(this.parkingLot).subscribe((data) => {
-      this.getParkingLotsList();
-      this.inputValue.reset();
-      this.numberInputValue.reset();
-      this.numberInputValue = new FormControl('1');
-    })
+      this.parkingService.addParkingLot(this.parkingLot).subscribe((data) => {
+        this.getParkingLotsList();
+        this.inputValue.reset();
+        this.numberInputValue.reset();
+        this.numberInputValue = new FormControl('1');
+      })
+    }
   }
 
   getParkingLotsList() {
@@ -104,18 +101,25 @@ export class ParkingComponent implements OnInit {
   }
 
   decrementParkingPlaceCount() {
-    if (this.numberInputValue.value != null) {
+    // @ts-ignore
+    if (this.numberInputValue.value > 1) {
+      // @ts-ignore
       this.numberInputValue.setValue((parseInt(this.numberInputValue.value) - 1).toString());
     }
   }
 
   changeNumberOfParkingLots(parking: ParkingLot) {
-    console.log(parking.parkingPlaceCount)
+    console.log(parking.parkingPlaceCount);
     // @ts-ignore
-    parking.parkingPlaceCount = parseInt(document.getElementById(parking.id).value);
-    this.parkingService.changeStatus(parking.id, parking).subscribe(response => {
-      console.log(parking);
-    })
+    if (parseInt(document.getElementById(parking.id).value) > 0) {
+      // @ts-ignore
+      parking.parkingPlaceCount = parseInt(document.getElementById(parking.id).value);
+      this.parkingService.changeStatus(parking.id, parking).subscribe(response => {
+        console.log(parking);
+      })
+    } else {
+      this.getParkingLotsList();
+    }
   }
 
 }
